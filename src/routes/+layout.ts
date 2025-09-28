361cf3faf3f9abf4f43d94a07f0d8d59170c5a70
blaccorek <jt.andriamihamina@gmail.com>
2025-09-28 15:42:59 +0000

    chore: render static pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,8 @@ import { A } from 'flowbite-svelte';
 
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = ({ route }) => {
     return {
         currentPage: route.id,
