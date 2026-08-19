4dbc5dcf5935226dbf806c26c9e39eb4e637e77a
blaccorek <jt.andriamihamina@gmail.com>
2026-08-19 20:15:24 +0200

    test: add artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,15 @@ const config: PlaywrightTestConfig = {
         port: 4173
     },
     testDir: 'tests',
-    testMatch: /^[\w\-./]*(test|spec)\.[jt]s$/
+    testMatch: /^[\w\-./]*(test|spec)\.[jt]s$/,
+    reporter: process.env.CI
+        ? [['github'], ['html', { open: 'never' }]]
+        : 'list',
+    retries: process.env.CI ? 2 : 0,
+    use: {
+        trace: 'on-first-retry',
+        screenshot: 'only-on-failure'
+    }
 };
 
 export default config;
